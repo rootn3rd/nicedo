4b6f5bda99d4cb97a95a02fda9238d4d6ca8e12f
sendevanshu <sendevanshu@gmail.com>
2016-10-31 20:02:52 +0530

    Updates
    1. Update the Reminder page UI to display the time difference and upcoming and pending events.

--- www/js/controllers/home.controller.js
+++ www/js/controllers/home.controller.js
@@ -9,40 +9,8 @@ angular.module('nicedo.controllers')
         $scope.item.date = 'Select date..';
         $scope.item.time = 'Select time..';
         $scope.dataList = [];
-        $scope.upcomingList = [];
-        $scope.pendingList = [];
-        if (window.localStorage.getItem("toDoList")) {
-            $scope.dataList = JSON.parse(window.localStorage.getItem("toDoList"));
-            if ($scope.dataList && $scope.dataList.length > 0) {
-                $scope.dataList.forEach(function (item) {
-                    if (item.actualDate && item.actualTime) {
-                        item.actualDate = new Date(item.actualDate);
-                        item.actualTime = new Date(item.actualTime);
-                        var tempDate = new Date(item.actualDate.getFullYear(), item.actualDate.getMonth(), item.actualDate.getDate(),
-                            item.actualTime.getHours(), item.actualTime.getMinutes(), item.actualTime.getSeconds());
-                        var currDate = new Date();
-                        if (currDate <= tempDate) {   //upcoming events
-                            var diffSeconds = (tempDate.getTime() - currDate.getTime()) / 1000;
-                            var sec_num = parseInt(diffSeconds, 10); // don't forget the second param
-                            var hours = Math.floor(sec_num / 3600) ? Math.floor(sec_num / 3600) + ' hours' : '';
-                            var mins = Math.floor((sec_num - (hours * 3600)) / 60) + ' mins';
-                            // var hours = diffSeconds/3600 ? diffDate.getHours() + ' hours' : '';
-                            // var mins = diffDate.getMinutes() + 'mins';
-                            item.dueInTime = hours + mins;
-                            $scope.upcomingList.push(item);
-                        }
-                        else {   //pending events
-                            var diffSeconds = (currDate.getTime() - tempDate.getTime()) / 1000;
-                            var sec_num = parseInt(diffSeconds, 10); // don't forget the second param
-                            var hours = Math.floor(sec_num / 3600) ? Math.floor(sec_num / 3600) + ' hours' : '';
-                            var mins = Math.floor((sec_num - (hours * 3600)) / 60) + ' mins';
-                            item.dueInTime = hours + mins;
-                            $scope.pendingList.push(item);
-                        }
-                    }
-                });
-            }
-        }
+        populateUpcomingPendingList();
+
         $ionicModal.fromTemplateUrl('templates/task-editor.html', {
             scope: $scope,
             animation: 'slide-in-up'
@@ -66,12 +34,14 @@ angular.module('nicedo.controllers')
             dataItem.actualTime = $scope.item.actualTime;
             $scope.dataList.push(dataItem);
             window.localStorage.setItem("toDoList", JSON.stringify($scope.dataList));
+
             var alertPopup = $ionicPopup.alert({
                 title: 'Message',
                 template: 'Reminder Saved'
             });
             alertPopup.then(function (res) {
                 $scope.modal.hide();
+                populateUpcomingPendingList();
             });
         }
         $ionicPlatform.ready(function () {
@@ -99,7 +69,7 @@ angular.module('nicedo.controllers')
                 };
 
                 $cordovaDatePicker.show(options).then(function (date) {
-                    $scope.item.actualDate = date.getMilliseconds();
+                    $scope.item.actualDate = date;
                     $scope.item.date = date.toLocaleDateString();
                 });
             }
@@ -148,8 +118,45 @@ angular.module('nicedo.controllers')
                     var mins = time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
                     var ampm = time.getHours() >= 12 ? 'PM' : 'AM';
                     $scope.item.time = hours + ":" + mins + " " + ampm;
-                    $scope.item.actualTime = time.getMilliseconds();
+                    $scope.item.actualTime = time;
                 });
             }
         });
+
+        function populateUpcomingPendingList() {
+            $scope.upcomingList = [];
+            $scope.pendingList = [];
+            if (window.localStorage.getItem("toDoList")) {
+                $scope.dataList = JSON.parse(window.localStorage.getItem("toDoList"));
+                if ($scope.dataList && $scope.dataList.length > 0) {
+                    $scope.dataList.forEach(function (item) {
+                        if (item.actualDate && item.actualTime) {
+                            item.actualDate = new Date(item.actualDate);
+                            item.actualTime = new Date(item.actualTime);
+                            var tempDate = new Date(item.actualDate.getFullYear(), item.actualDate.getMonth(), item.actualDate.getDate(),
+                                item.actualTime.getHours(), item.actualTime.getMinutes(), item.actualTime.getSeconds());
+                            var currDate = new Date();
+                            if (currDate <= tempDate) {   //upcoming events
+                                var diffSeconds = (tempDate.getTime() - currDate.getTime()) / 1000;
+                                var sec_num = parseInt(diffSeconds, 10); // don't forget the second param
+                                var hours = Math.floor(sec_num / 3600) ? Math.floor(sec_num / 3600) + ' hours' : '';
+                                var mins = Math.floor((sec_num - (hours * 3600)) / 60) + ' mins';
+                                // var hours = diffSeconds/3600 ? diffDate.getHours() + ' hours' : '';
+                                // var mins = diffDate.getMinutes() + 'mins';
+                                item.dueInTime = hours + mins;
+                                $scope.upcomingList.push(item);
+                            }
+                            else {   //pending events
+                                var diffSeconds = (currDate.getTime() - tempDate.getTime()) / 1000;
+                                var sec_num = parseInt(diffSeconds, 10); // don't forget the second param
+                                var hours = Math.floor(sec_num / 3600) ? Math.floor(sec_num / 3600) + ' hours' : '';
+                                var mins = Math.floor((sec_num - (hours * 3600)) / 60) + ' mins';
+                                item.dueInTime = hours + mins;
+                                $scope.pendingList.push(item);
+                            }
+                        }
+                    });
+                }
+            }
+        }
     });
